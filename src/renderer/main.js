524f07278470200767594a39cbfba2eea73d623a
dineug <dineug2@gmail.com>
2019-04-18 21:16:00 +0900

    sentry add

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -5,6 +5,7 @@ import App from './App'
 import router from './router'
 import store from './store'
 import './js/fontawesome'
+import './js/sentry'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
